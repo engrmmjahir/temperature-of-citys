03dc9b051c3c3e746a59a03eb98c6c912c7a0e77
Jahir <engrmmjahir@gmail.com>
2022-09-04 14:51:56 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// DO NOT Show API KEY on your JS FILE
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+// const API_KEY = `e77b4d9db8f7bcca23671ff78f1251fb`;
+
+const loadTemperature = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const displayTemperature = data => {
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city 
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
+
+loadTemperature('dhaka')
+// const searchTemperature = () => {
+
+// }
